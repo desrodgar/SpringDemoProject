339a63f5aa3dec5d2b1aaf05ab4160741b11da69
desrodgar <desrodgar@gmail.com>
2021-12-17 12:27:21 +0100

    Third commit solving error

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module mssc.brewery {
+    requires spring.boot.actuator.autoconfigure;
+    requires spring.boot.autoconfigure;
+    requires lombok;
+    requires spring.web;
+    requires spring.context;
+}
